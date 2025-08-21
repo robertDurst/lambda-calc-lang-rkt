32919b469e3e2df706310d866e2fc5254f463872
robertDurst <me@robdurst.com>
2025-08-21 13:17:20 -0600

    refactor: encapsulate environment bindings in dedicated struct and update related functions

--- src/type-inference.rkt
+++ src/type-inference.rkt
@@ -1,30 +1,18 @@
 #lang typed/racket
 (require racket/dict)
 (require "common/terms.rkt")
-(provide type-inference fresh-type-variable lookup-from-env infer-variable)
+(provide type-inference environment fresh-type-variable lookup-from-env infer-variable)
 
 ;; Public
 (struct type-inference ([counter : Integer]) #:transparent)
+(struct environment ([bindings : (Listof (Pairof String String))]) #:transparent)
 
-(: fresh-type-variable (-> type-inference (Pairof String type-inference)))
-(define (fresh-type-variable type-inference)
-  (cons (format "T~a" (type-inference-counter type-inference)) (increment-type-inference type-inference)))
-
-(: infer-variable (-> type-inference (Listof (Pairof String String)) variable String))
+(: infer-variable (-> type-inference environment variable String))
 (define (infer-variable type-inference environment variable)
   (define variable-type (lookup-from-env environment (variable-name variable)))
   (if (empty? variable-type)
       (error "Variable not found")
       variable-type))
-
-(: lookup-from-env (-> (Listof (Pairof String String)) String String))
-(define (lookup-from-env env var)
-  (cond
-    [(empty? env) (error "Variable not found")]
-    [(equal? (car (first env)) var) (cdr (first env))]
-    [else (lookup-from-env (rest env) var)]
-    )
-  )
 
 ;; Private
 (: make-type-inference (-> Integer type-inference))
@@ -33,3 +21,17 @@
 (: increment-type-inference (-> type-inference type-inference))
 (define (increment-type-inference type-inference)
   (make-type-inference (+ 1 (type-inference-counter type-inference))))
+
+(: lookup-from-env (-> environment String String))
+(define (lookup-from-env env var)
+  (define bindings (environment-bindings env))
+  (cond
+    [(empty? bindings) (error "Variable not found")]
+    [(equal? (car (first bindings)) var) (cdr (first bindings))]
+    [else (lookup-from-env (environment (rest bindings)) var)]
+    )
+  )
+
+(: fresh-type-variable (-> type-inference (Pairof String type-inference)))
+(define (fresh-type-variable type-inference)
+  (cons (format "T~a" (type-inference-counter type-inference)) (increment-type-inference type-inference)))
