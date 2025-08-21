10800329dfc9d9ca3d2290afbbeeeca20de6f999
robertDurst <me@robdurst.com>
2025-08-21 12:02:38 -0600

    feat: implement lambda calculus parsing with variable, abstraction and application terms

--- compiler.rkt
+++ compiler.rkt
@@ -2,13 +2,41 @@
 
 (require "string-utils.rkt")
 
+(provide parse parse-term variable abstraction application)
+
+(struct variable ([name : String]) #:transparent)
+(struct abstraction ([variable : String] [body : String]) #:transparent)
+(struct application ([left : String] [right : String]) #:transparent)
+
+(: parse (-> String (Listof Any)))
+(define (parse input)
+  (define term-strings (string-split input "\n"))
+  (map parse-term term-strings)
+  )
 
 ;; e ::= x ∣ λx.e ∣ e1 e2
-; (: lexer (-> String (Listof String)))
-; (define (lexer input)
-;   (cond
-;     [string=? input '()]
-;     [else (cons (first input) (lexer (rest input)))])
-;   )
+(: parse-term (-> String Any))
+(define (parse-term input)
+  (define first-char (substring input 0 1))
+  (cond
+    [(equal? first-char "λ") (parse-abstraction input)]
+    [(equal? (string-replace input " " "") input) (parse-variable input)]
+    [else (parse-application input)]
+    )
+  )
 
+(: parse-variable (-> String variable))
+(define (parse-variable input)
+  (variable input))
+
+(: parse-abstraction (-> String abstraction))
+(define (parse-abstraction input)
+  (define non-lambda-input (substring input 1 (string-length input)))
+  (define split-on-dot (string-split non-lambda-input "."))
+  (abstraction (first split-on-dot) (second split-on-dot)))
+
+(: parse-application (-> String application))
+(define (parse-application input)
+  (define split-input (split input))
+  (application (first split-input) (second split-input)))
 
--- test-compiler.rkt
+++ test-compiler.rkt
@@ -5,11 +5,15 @@
 (define parse-tests
   (test-suite
    "Parse Tests"
-   ;  (test-case "lexer empty returns empty list"
-   ;             (check-equal? (lexer "") '()))
-   ;  (test-case "lexer single element returns list with single element"
-   ;             (check-equal? (lexer "x") '("x")))
-
+   (test-case "parse abstraction"
+              (check-equal? (parse-term "λx.e") (abstraction "x" "e")))
+   (test-case "parse application"
+              (check-equal? (parse-term "e1 e2") (application "e1" "e2")))
+   (test-case "parse variable"
+              (check-equal? (parse-term "x") (variable "x")))
+   (test-case "parses multiple terms"
+              (check-equal? (parse "x\ny\nλx.e\ne1 e2")
+                            (list (variable "x") (variable "y") (abstraction "x" "e") (application "e1" "e2"))))
    )
   )
 
